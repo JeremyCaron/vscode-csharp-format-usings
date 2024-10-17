be2b1249187dc1f4526103e18c9e042063e7ddb9
Jeremy Caron <jeremy@allocate.co>
2024-10-17 18:35:00 -0400

    #12: clean up the src folder a bit by combining some src files, rename some stuff

--- src/formatting.ts
+++ src/formatting.ts
@@ -1,4 +1,5 @@
 import * as vs from 'vscode';
+import { log } from "./logger";
 
 export interface IFormatOptions {
     sortOrder: string;
@@ -12,6 +13,36 @@ export interface IResult {
     source?: string;
     error?: string;
 }
+const getFormatOptions = (): IFormatOptions => {
+    const cfg = vs.workspace.getConfiguration('csharpOrganizeUsings');
+
+    return {
+        sortOrder: cfg.get<string>('sortOrder', 'System Microsoft'),
+        splitGroups: cfg.get<boolean>('splitGroups', true),
+        removeUnnecessaryUsings: cfg.get<boolean>('removeUnnecessaryUsings', true),
+        numEmptyLinesAfterUsings: cfg.get<number>('numEmptyLinesAfterUsings', 1),
+        numEmptyLinesBeforeUsings: cfg.get<number>('numEmptyLinesBeforeUsings', 1),
+    };
+};
+
+export async function getEdits(editor: vs.TextEditor, edit: vs.TextEditorEdit) {
+    log("Extension executed!!");
+    var options = getFormatOptions();
+
+    try {
+        var result = process(editor, options);
+        if (result) {
+            const range = new vs.Range(new vs.Position(0, 0), editor.document.lineAt(editor.document.lineCount - 1).range.end);
+            edit.delete(range);
+            edit.insert(new vs.Position(0, 0), result);
+        }
+    }
+    catch (ex) {
+        let message = 'Unknown Error';
+        if (ex instanceof Error) message = ex.message;
+        vs.window.showWarningMessage(message);
+    }
+};
 
 declare type Func<T, S> = (...args: S[]) => T;
 
